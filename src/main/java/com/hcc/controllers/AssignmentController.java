bf235ca9a92702dca16ed4b74f3f55e80ce0b190
Andy Pan <andybpan@gmail.com>
2024-05-23 21:49:28 -0700

    authentication & assignment controller set up

---
+++ src/main/java/com/hcc/controllers/AssignmentController.java
@@ -0,0 +1,40 @@
+package com.hcc.controllers;
+
+import com.hcc.entities.Assignment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/assignments")
+public class AssignmentController {
+
+//    private final AssignmentService assignmentService;
+//
+//    @Autowired
+//    public AssignmentController(AssignmentService assignmentService) {
+//        this.assignmentService = assignmentService;
+//    }
+//
+//    @GetMapping
+//    public ResponseEntity<List<Assignment>> getAllAssignmentsByUser(@RequestParam String userId) {
+//        return ResponseEntity.ok(assignmentService.getAssignmentsByUser(userId));
+//    }
+//
+//    @GetMapping("/{id}")
+//    public ResponseEntity<Assignment> getAssignmentById(@PathVariable Long id) {
+//        return ResponseEntity.ok(assignmentService.getAssignmentById(id));
+//    }
+//
+//    @PutMapping("/{id}")
+//    public ResponseEntity<Assignment> updateAssignmentById(@PathVariable Long id, @RequestBody Assignment assignment) {
+//        return ResponseEntity.ok(assignmentService.updateAssignmentById(id, assignment));
+//    }
+//
+//    @PostMapping
+//    public ResponseEntity<Assignment> createAssignment(@RequestBody Assignment assignment) {
+//        return ResponseEntity.ok(assignmentService.createAssignment(assignment));
+//    }
+}
---
+++ src/main/java/com/hcc/controllers/AuthenticationController.java
@@ -0,0 +1,29 @@
+package com.hcc.controllers;
+
+import com.hcc.dto.AuthCredentialsRequest;
+import com.hcc.services.UserDetailServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthenticationController {
+
+//    private final AuthenticationService authenticationService;
+//
+//    @Autowired
+//    public AuthenticationController(AuthenticationService authenticationService) {
+//        this.authenticationService = authenticationService;
+//    }
+//
+//    @PostMapping("/login")
+//    public ResponseEntity<?> login(@RequestBody AuthCredentialsRequest request) {
+//        return ResponseEntity.ok(authenticationService.login(request));
+//    }
+//
+//    @GetMapping("/validate")
+//    public ResponseEntity<?> validateToken(@RequestParam String token) {
+//        return ResponseEntity.ok(authenticationService.validateToken(token));
+//    }
+}
