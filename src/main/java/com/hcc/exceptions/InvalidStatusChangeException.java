b4e78e1c96aa646ca86179ce8ca811dd5f1928eb
Andy Pan <andybpan@gmail.com>
2024-05-26 21:28:53 -0700

    update exceptions

--- src/main/java/com/hcc/exceptions/InvalidStatusChangeException.java
+++ src/main/java/com/hcc/exceptions/InvalidStatusChangeException.java
@@ -3,7 +3,7 @@ package com.hcc.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND)
+@ResponseStatus(value = HttpStatus.CONFLICT)
 public class InvalidStatusChangeException extends RuntimeException{
 
     private static final long serialVersionUID = 2L;
