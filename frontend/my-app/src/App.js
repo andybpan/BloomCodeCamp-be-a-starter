5b5df64b2bc771a699ac4a6accd34b5aeb63866d
Andy Pan <andybpan@gmail.com>
2024-05-28 19:50:56 -0700

    added react-router-dom

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,26 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          - (^O wO^)
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "App">
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/Login">Login</Link></li>
+              <li><Link to="/Dashboard">Dashboard</Link></li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/Login" element={<Login />} />
+            <Route path="/Dashboard" element={<Dashboard />} />
+          </Routes>
+        </div>
+      </Router>
     </div>
+
   );
 }
+
+//function App() {
+//  return (
+//    <div className="App">
+//      <header className="App-header">
+//        <img src={logo} className="App-logo" alt="logo" />
+//        <p>
+//          Edit <code>src/App.js</code> and save to reload.
+//        </p>
+//        <a
+//          className="App-link"
+//          href="https://reactjs.org"
+//          target="_blank"
+//          rel="noopener noreferrer"
+//        >
+//          Learn React
+//        </a>
+//        <Routes>
+//          <Route exact path="/" component={Home} />
+//          <Route path="/login" component={Login} />
+//          <Route path="/dashboard" component={Dashboard} />
+//        </Routes>
+//
+//        </header>
+//    </div>
+//  );
+//}
 
 export default App;
---
+++ frontend/my-app/src/components/Login.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Login() {
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <form>
+        <label>
+          Username:
+          <input type="text" name="username" />
+        </label>
+        <label>
+          Password:
+          <input type="password" name="password" />
+        </label>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
