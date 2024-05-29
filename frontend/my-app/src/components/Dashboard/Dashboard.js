decaaf7c7969118d355d9186d4725c95c78f419a
Andy Pan <andybpan@gmail.com>
2024-05-28 21:24:38 -0700

    Learner's Dashboard

--- frontend/my-app/src/components/Dashboard/Dashboard.js
+++ frontend/my-app/src/components/Dashboard/Dashboard.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import './Dashboard.css';
 
 function Dashboard() {
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <p>Welcome to the Dashboard</p>
+    <div className="Dashboard">
+      <header className="Dashboard-header">
+        <h1>Learner's Dashboard</h1>
+      </header>
+      <button className="logout-button">Log Out</button>
+      <main className="Dashboard-main">
+        <section className="container in-review">
+          <h2 className="container-title">In Review</h2>
+        </section>
+        <section className="container submitted-resubmitted">
+          <h2 className="container-title">Submitted & Resubmitted</h2>
+        </section>
+        <section className="container completed">
+          <h2 className="container-title">Completed</h2>
+        </section>
+      </main>
+      <footer className="Dashboard-footer">
+        <p>Hello World!</p>
+      </footer>
     </div>
   );
 }
