171d3424ff39c046318b8374c063b7c99cc41096
Andy Pan <andybpan@gmail.com>
2024-05-28 21:41:33 -0700

    clean up app.js

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -27,31 +27,4 @@ function App() {
   );
 }
 
-//function App() {
-//  return (
-//    <div className="App">
-//      <header className="App-header">
-//        <img src={logo} className="App-logo" alt="logo" />
-//        <p>
-//          Edit <code>src/App.js</code> and save to reload.
-//        </p>
-//        <a
-//          className="App-link"
-//          href="https://reactjs.org"
-//          target="_blank"
-//          rel="noopener noreferrer"
-//        >
-//          Learn React
-//        </a>
-//        <Routes>
-//          <Route exact path="/" component={Home} />
-//          <Route path="/login" component={Login} />
-//          <Route path="/dashboard" component={Dashboard} />
-//        </Routes>
-//
-//        </header>
-//    </div>
-//  );
-//}
-
 export default App;
