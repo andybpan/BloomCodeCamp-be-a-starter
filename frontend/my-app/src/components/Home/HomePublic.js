b0e568006e7400079552361c9a6b03e2e68c88fe
Andy Pan <andybpan@gmail.com>
2024-06-15 13:20:43 -0700

    new structure

---
+++ frontend/my-app/src/components/Home/HomePublic.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="home-container">
+      <header className="home-header">
+        Welcome to the Assignment Review App
+      </header>
+      <main className="home-main">
+        <Link to="/Login" className="login-button">
+          Login
+        </Link>
+        <p className="home-description">
+          Hello! Login to view your dashboard!
+        </p>
+      </main>
+      <footer className="home-footer">
+      </footer>
+    </div>
+  );
+}
+
+export default Home;
