991b4dc317191cf13588f0fc8c63cf179deea2ce
Andy Pan <andybpan@gmail.com>
2024-06-29 15:53:32 -0700

    cleaned up home authenticated page and added navigation dashboard logic

--- frontend/my-app/src/components/Home/HomeAuthenticated.js
+++ frontend/my-app/src/components/Home/HomeAuthenticated.js
@@ -16,27 +16,39 @@ function Home() {
     return null;
   };
 
+  // Function handles dashboard navigation based on user's authority
+  // double check role keywords
+  const handleDashboard = () => {
+    const role = getRoleFromToken();
+    if (role === 'leaner') {
+      navigateToDashboard('/LearnerDashboard');
+    } else if (role === 'reviewer') {
+      navigateToDashboard('/ReviewerDashboard');
+    } else {
+      // Handle cases where the role is not defined or token is invalid
+      // Maybe redirect to a login page or show an error message
+      // for now: just shows error message
+      console.log('Role not defined or token is invalid.');
+    }
+  };
+
   return (
     <div className="home-container">
       <header className="home-header">
         Welcome to the Assignment Review App
       </header>
       <main className="home-main">
-        {/*
-          HomeAuthenticated needs to have a dashboard button to link to correct dashboard
-          1. link button to Dashboard
-          2. Create logic to link to correct dashboard - based on token?
-        */}
         <button onClick={handleDashboard} className="login-button">
           Go to my Dashboard
         </button>
         <p className="home-description">
-          Hello! Click to go to you Dashboard!
+          Hello! Click to go to your Dashboard!
         </p>
       </main>
       <footer className="home-footer">
       </footer>
     </div>
+  );
   );
 }
 
